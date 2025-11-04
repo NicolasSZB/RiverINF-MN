235e57a3c4d34119a4001171e5473a72bc11386e
Nicolas de Souza Brasil <nicolassouzabrasil@gmail.com>
2025-11-04 18:13:31 -0300

    Initial commit
    Commit inicial introduzindo o programa main "main.c" e o primeiro level para teste

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "raylib.h"
+
+#define LARGURA 800
+#define ALTURA 450
+
+int main(){
+
+    InitWindow(LARGURA, ALTURA, "RiverINF");
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+
+    }
+}
