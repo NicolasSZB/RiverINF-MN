ad7b81a560498f2c0eb75a3a76e9444eacbc75a4
Matheus030205 <matheustessmann03@gmail.com>
2025-11-13 12:53:29 -0300

    Save changes

--- src/.c e. h contendo as funcoes (pasta temporaria)/sprites.c
+++ src/.c e. h contendo as funcoes (pasta temporaria)/sprites.c
@@ -1,85 +1,91 @@
 #include "raylib.h"
+#include "sprite.h"
 
-#define TAMANHO_PIXEL 57.0f
-#define ARQUIVOSPRITE "C:\\Users\\mathe\\OneDrive\\Imagens\\sprites.png"
-#define LARGURA 800
-#define ALTURA 450
+#define ARQUIVOSPRITE "sprites.png"
 
-int main(void){
 
-    InitWindow(LARGURA,ALTURA,"TESTE SPRITE");
-    SetTargetFPS(60);
+SpriteSheet Carregarsprites(void){
+    SpriteSheet sheet = {0};
 
-    Texture2D arquivo = LoadTexture(ARQUIVOSPRITE);
+    sheet.arquivo = LoadTexture(ARQUIVOSPRITE);
 
-    Rectangle jogador_aviao_reto = {
-            101.0f,68.0f,57,57// PEGA PRIMEIRO AVIAO DO INDO RETO
-            };
+//PLAYER & BULLET
 
-    Rectangle jogador_aviao_direita = {
-        169.0f,71.0f,42,58
-    };
+    sheet.jogador_aviao_reto = {101.0f,68.0f,57,57};
+    sheet.jogador_aviao_direita = {169.0f,71.0f,42,58};
 
-    Rectangle jogador_aviao_esquerda= {
-        49.0f,74.0f,42,56
-    };
+    sheet.jogador_aviao_esquerda= {49.0f,74.0f,42,56};
 
-    Rectangle bala = {
-        22.0f,85.0f,9,32
-    };
+    sheet.bala = {22.0f,85.0f,9,32};
 
-    Rectangle helicoptero = {
-        10.0f,184.0f,64,41
-    };
-    Rectangle explosao_amarela = {
-        30.0f,314.0f,56,44
-    };
-    Rectangle fuel = {
-        611.0f,61.0f,56,96
-    };
 
-    Rectangle ponte_nivel = {
-        685.0f,64.0f,254,89
+// ENEMIES
+
+    sheet.helicoptero = {10.0f,184.0f,64,41};
+    sheet.jato_inimigo = {159.0f,197.0f,65,25};
+    sheet.lancha = {15.0f,234.0f,128,32};
+
+// EFFECTS
+    sheet.fuel = {611.0f,61.0f,56,96};
+    sheet.explosao_amarela = {30.0f,314.0f,56,44};
+
+    // BACKGROUND
+
+    sheet.ponte_nivel = {685.0f,64.0f,254,89};
+    sheet.estrada = {267.0f,58.0f,64,104};
+
+    sheet.campo_verde = {335.0f,62.0f,126,95};
+
+    sheet.hipotenusa_esquerda = {335.0f,25.0f,127,32};
+
+    sheet.hipotenusa_esquerda_invertida = {334.0f,162.0f,128,32};
+
+    sheet.hipotenusa_direita = {467.0f,25.0f,128,32};
+
+    sheet.hipotenusa_direita_invertida = {467.0f,161.0f,128,32};
+
+    sheet.campo_verde_casa = {467.0f,62.0f,128,96};
+
+
+    //INTERFACE
+
+    sheet.fuel_bar = {295.0f,265.0f,312,61};
+
+    sheet.activision_logo = {350.0f,337.0f,257,28};
+
+    sheet.um= {294.0f,385.0f,32,32};
+    sheet.dois= {336.0f,385.0f,48,32};
+    sheet.tres= {390.0f,385.0f,48,32};
+    sheet.quatro= {446.0f,385.0f,48,32};
+    sheet.cinco= {502.0f,385.0f,48,32};
+    sheet.seis= {558.0f,385.0f,48,32};
+    sheet.sete= {615.0f,385.0f,48,32};
+    sheet.oito= {670.0f,385.0f,48,32};
+    sheet.nove= {726.0f,385.0f,48,32};
+    sheet.zero={782.0f,385.0f,48,32};
+
+    return sheet;
+}
+
+void drawing_sprite(SpriteSheet *sprite,Rectangle sprite_corte,float x,float y){
+   const float escala= 1.0f;
+   const float rotacao = 0.0f;
+
+   Rectangle sprite_cortado = sprite_corte;
+
+   float largura_final = sprite_cortado.width*escala;
+   float altura_final = sprite_cortado.height*escala;
+
+   Rectangle destino={x,y,largura_final,altura_final};
+
+   Vector2 origem = {0.0f,0.0f};
+
+   DrawTexturePro(sprite->arquivo,sprite_cortado,destino,origem,rotacao,WHITE);
+
+}
+
+void unload_sprite(SpriteSheet *sheet){
+    if(sheet != false && sheet->arquivo.id != 0){
+        UnloadTexture(sheet.arquivo);
     }
-    Rectangle estrada = {
-        267.0f,58.0f,64,104
-    };
-
-    Rectangle campo_verde = {
-        335.0f,62.0f,126,95
-    };
-
-    Rectangle hipotenusa_esquerda = {
-        335.0f,25.0f,127,32
-    };
-
-    Rectangle hipotenusa_esquerda_invertida = {
-        334.0f,162.0f,128,32
-    };
-
-    Rectangle explosao_amarela = {
-        30.0f,314.0f,56,44
-    };
- /*   Vector2 posicao_aviao_reto = {
-            (float)LARGURA/2 - TAMANHO_PIXEL/2,
-            (float)ALTURA  - TAMANHO_PIXEL - 20
-        };// POSICAO ONDE VAI FICAR NA JANELA DO JOGO
-    Vector2 posicao_aviao_direita = {
-            (float)LARGURA/2 - 58,
-            (float)ALTURA  - 30
-        };// POSICAO ONDE VAI FICAR NA JANELA DO JOGO
-*/
-       while(!WindowShouldClose()){
-            BeginDrawing();
-            ClearBackground(BLUE);
-
-        //DrawTextureRec(arquivo,jogador_aviao_reto,posicao_aviao_reto,YELLOW);
-        //    DrawTextureRec(arquivo,jogador_aviao_direita,posicao_aviao_direita,YELLOW);
-
-            EndDrawing();
-       }
-       UnloadTexture(arquivo);
-       CloseWindow();
-
-       return 0;
 }
